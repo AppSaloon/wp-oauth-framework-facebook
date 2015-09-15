d7a0730b5582aa945f5e3e44d4144c7c395442fb
Koen Gabriëls <koeng@appsaloon.be>
2015-09-15 11:06:41 +0200

    Adds proper config for Facebook Oauth service

--- wp-oauth-framework-facebook.php
+++ wp-oauth-framework-facebook.php
@@ -11,7 +11,16 @@
 add_filter( 'wpof_registered_services', 'fbl_add_service' );
 
 function fbl_add_service( $registered_services ) {
-    $registered_services[] = new \wp_oauth_framework\classes\Oauth_Service( 'Facebook' );
+    $config = array(
+        'authorize_endpoint' => 'https://www.facebook.com/dialog/oauth',
+        'token_endpoint' => 'https://graph.facebook.com/v2.3/oauth/access_token',
+        'credentials_in_request_body' => true,
+        'use_comma_separated_scope' => false,
+        'user_info_endpoint' => 'https://graph.facebook.com/v2.4/me?fields=id,name,email',
+        'user_info_endpoint_method' => 'get',
+        'scope' => array( 'email' ),
+    );
+    $registered_services[] = new \wp_oauth_framework\classes\Oauth_Service( 'Facebook', $config );
     return $registered_services;
 }
 
